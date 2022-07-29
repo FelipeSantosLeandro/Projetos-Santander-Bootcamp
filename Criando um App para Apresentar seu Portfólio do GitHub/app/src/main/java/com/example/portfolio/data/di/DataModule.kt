b54d2d480710d318151130fd3e1803e543ce2c23
Felipe Santos <106402988+FelipeSantosLeandro@users.noreply.github.com>
2022-07-29 12:03:26 -0300

    Create DataModule.kt

---
+++ Criando um App para Apresentar seu Portfólio do GitHub/app/src/main/java/com/example/portfolio/data/di/DataModule.kt
@@ -0,0 +1,61 @@
+package com.example.portflio.data.di
+
+import android.util.Log
+import com.example.portflio.data.di.services.GitHubServices
+import com.example.portflio.data.repositores.RepoRepository
+import com.example.portflio.data.repositores.RepoRepositoryImpl
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object DataModule {
+
+    private const val OK_HTTP = "OKHTTP"
+
+    fun load() {
+        loadKoinModules(networkModules() + repositoriesModule())
+    }
+
+    private fun networkModules(): Module {
+        return module {
+            single {
+                val interceptor = HttpLoggingInterceptor {
+                    Log.e(OK_HTTP, it,)
+                }
+                interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+                OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+            }
+
+            single {
+                GsonConverterFactory.create(GsonBuilder().create())
+            }
+
+            single {
+                createServices<GitHubServices>(get(),get())
+            }
+        }
+    }
+
+    private fun repositoriesModule(): Module {
+        return module {
+            single<RepoRepository> { RepoRepositoryImpl(get()) }
+        }
+    }
+
+    private inline fun <reified T> createServices(client: OkHttpClient, factory: GsonConverterFactory) : T {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .client(client)
+            .addConverterFactory(factory)
+            .build().create(T::class.java)
+    }
+}
